5e95026d3bcf5fa5159e9832a83889f6ce3bc4d5
Deepu kumar <deeputhakur0986@gmail.com>
2025-12-31 14:19:22 +0530

    I setup the basic express server

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
 
 import dotenv from "dotenv";
-
+import express from "express";
 
 dotenv.config({
     path: "./.env"
 });
 
-const myusername = process.env.username;
-console.log("value:", myusername);
+// const myusername = process.env.username;
+// console.log("value:", myusername);
 
-const mydatabasename = process.env.database;
-console.log("database: ", mydatabasename);
+// const mydatabasename = process.env.database;
+// console.log("database: ", mydatabasename);
 
-console.log("Started of backend project");
+// console.log("Started of backend project");
+
+
+
+// const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello Bhole !')
+});
+
+app.get("/instagram", (req, res) => {
+    res.send("This is an instagram page!");
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+});
+
