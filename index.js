b416a7ad0e926b583ccf4f2f14a7e5b10cf7d3b9
Deepu kumar <deeputhakur0986@gmail.com>
2025-12-31 11:35:24 +0530

    I ininitialize the dotenv package for my backend project

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+
+import dotenv from "dotenv";
+
+
+dotenv.config({
+    path: "./.env"
+});
+
+const myusername = process.env.username;
+console.log("value:", myusername);
+
+const mydatabasename = process.env.database;
+console.log("database: ", mydatabasename);
+
 console.log("Started of backend project");
