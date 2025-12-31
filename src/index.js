7d42f3d44985d0334e59260b0a89709e7c8d27ae
Deepu kumar <deeputhakur0986@gmail.com>
2025-12-31 15:11:40 +0530

    I used the postman for API testing

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello Bhole !')
+  res.send('Hello Bhole!')
 });
 
 app.get("/instagram", (req, res) => {
